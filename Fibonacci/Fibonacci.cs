f715599ec9031a371423d287dd9dd6e446a1ca5a
Victor Dragonici <victor.dragonici@digitalcodify.com>
2023-10-17 11:53:04 +0300

    incarcare solutie Fibonacci cu recursivitate

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Fibonacci
+{
+    public static List<int> Generate(int N)
+    {
+        List<int> sequence = new List<int>();
+
+        if (N <= 0)
+        {
+            return sequence;
+        }
+
+        int a = 0, b = 1;
+        sequence.Add(a);
+
+        while (b <= N)
+        {
+            sequence.Add(b);
+
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return sequence;
+    }
+}
+
+
+   
+
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FibonacciProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //preluam input de la utilizator
+            Console.WriteLine("Introduceti un numar pentru a genera secventa Fibonacci pana la acesta:");
+            int N;
+
+
+            //ne asiguram ca utilizatorul introduce un numar intreg valid
+            // verificam si daca este numar pozitiv
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out N) && N >= 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Va rugam să introduceti un numar întreg pozitiv.");
+                }
+            }
+
+            Console.WriteLine($"Secventa Fibonacci pana la {N}:");
+            foreach (int num in Fibonacci.Generate(N))
+            {
+                Console.Write(num + " ");
+            }
+
+            // doar ca sa imi ramana deschis terminalul
+            Console.ReadLine();
+        }
+    }
+}
